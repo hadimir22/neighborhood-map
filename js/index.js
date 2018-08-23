6da6197eb674284ab40ed1c8d08bc9a89607ef45
hadimir22 <hadimir22@gmail.com>
2018-08-23 11:57:33 -0700

    add more

---
+++ js/index.js
@@ -0,0 +1,109 @@
+var winheight = $( window ).height(); 
+$("#map").css("min-height", winheight-80);
+ 
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+// Global Variables
+var map;
+
+function AppViewModel() {
+    var self = this;
+
+    this.search = ko.observable("");
+    this.markers = [];
+
+    // This function populates the infowindow when the marker is clicked. We'll only allow
+    // one infowindow which will open at the marker that is clicked, and populate based
+    // on that markers position.
+    this.populateInfoWindow = function(marker, infowindow) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
+          infowindow.marker = marker;
+          infowindow.setContent('<div>' + marker.title + '</div>');
+          infowindow.open(map, marker);
+          // Make sure the marker property is cleared if the infowindow is closed.
+          infowindow.addListener('closeclick',function(){
+            infowindow.setMarker = null;
+          });
+        }
+    };
+
+    this.populateAndBounceMarker = function() {
+        self.populateInfoWindow(this, self.largeInfoWindow);
+        this.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout((function() {
+            this.setAnimation(null);
+        }).bind(this), 1400);
+    };
+
+    this.initMap = function() {
+        var mapCanvas = document.getElementById('map');
+        var mapOptions = {
+            center: new google.maps.LatLng(34.083656, 74.797371),
+            zoom: 12, 
+        };
+        // Constructor creates a new map - only center and zoom are required.
+        map = new google.maps.Map(mapCanvas, mapOptions);
+
+        // Set InfoWindow
+        this.largeInfoWindow = new google.maps.InfoWindow();
+        for (var i = 0; i < myLocations.length; i++) {
+            this.markerTitle = myLocations[i].title;
+            this.markerLat = myLocations[i].lat;
+            this.markerLng = myLocations[i].lng;
+            // Google Maps marker setup
+            this.marker = new google.maps.Marker({
+                map: map,
+                position: {
+                    lat: this.markerLat,
+                    lng: this.markerLng
+                },
+                title: this.markerTitle,
+                lat: this.markerLat,
+                lng: this.markerLng,
+                id: i,
+                animation: google.maps.Animation.DROP
+            });
+            this.marker.setMap(map);
+            this.markers.push(this.marker);
+            this.marker.addListener('click', self.populateAndBounceMarker);
+        }
+    };
+
+    this.initMap();
+
+    // This block appends our locations to a list using data-bind
+    // It also serves to make the filter work
+    this.myLocationsFilter = ko.computed(function() {
+        var result = [];
+        for (var i = 0; i < this.markers.length; i++) {
+            var markerLocation = this.markers[i];
+            if (markerLocation.title.toLowerCase().includes(this.search()
+                    .toLowerCase())) {
+                result.push(markerLocation);
+                this.markers[i].setVisible(true);
+            } else {
+                this.markers[i].setVisible(false);
+            }
+        }
+        return result;
+    }, this);
+}
+
+googleError = function googleError() {
+    alert(
+        'coudnt load! Please refresh the page and try again!'
+    );
+};
+
+function startApp() {
+    ko.applyBindings(new AppViewModel());
+}
---
+++ js/markers.js
@@ -0,0 +1,28 @@
+var myLocations = [
+  {
+      title: 'dal lake', 
+       lat:34.1166662,
+       lng:74.8666632
+    },
+  {
+      title: 'negeen lake',  
+      lat:34.113889,
+       lng: 74.832222
+    },
+  {
+      title: 'Dargah hazratbal', 
+        lat: 34.129167,
+        lng: 74.842222
+    },
+  {
+      title: 'tulip garden',
+         lat:34.096056, 
+         lng: 74.88003
+        },
+  {
+      title: 'Jamia musjid', 
+        lat:34.098352,
+        lng: 74.80918
+        
+        }
+];
